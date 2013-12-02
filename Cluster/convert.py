10ee1ff50c71fcd84b44c78ba96ad280f5592bab
adam warner <adam.r.warner@gmail.com>
2013-12-02 14:27:33 -0500

    first commit, setup system

---
+++ Cluster/convert.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+import os
+
+if len(sys.argv) < 4:
+    sys.exit("Usage: convert.py ~/dir .tsv .csv")
+else:    
+    cur_dir, old_ext, new_ext = sys.argv[1:]
+    for filename in os.listdir(cur_dir):
+        filename = os.path.join(cur_dir, filename)
+        base, file_ext = os.path.splitext(filename)
+        if file_ext == old_ext:
+            newfile = base + new_ext
+            csv.field_size_limit(sys.maxsize)
+            with open(filename, 'rb') as ifh, open(newfile, 'wb') as ofh:
+                reader = csv.reader(ifh, delimiter='\t')
+                csv.writer(ofh, delimiter='|').writerows(reader)
+            os.unlink(filename)
